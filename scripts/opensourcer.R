3edf4753557e805414136bc36838c5e4e5d47751
Florian Drinkler <flo.drinkler@gmail.com>
2020-12-24 11:57:03 +0100

    :sparkles: add Contributing to Open Source graph

---
+++ scripts/opensourcer.R
@@ -0,0 +1,35 @@
+library(ggplot2)
+library(DBI)
+library(scales)
+
+# Connect to database
+con <- dbConnect(RSQLite::SQLite(), "res/survey_results.db")
+
+# Return countries with number of participants
+opensourcer <- dbGetQuery(con,
+                          "SELECT OpenSourcer, COUNT(OpenSourcer) AS Amount
+                          FROM survey
+                          WHERE OpenSourcer IS NOT NULL
+                          GROUP BY OpenSourcer
+                          ORDER BY Amount DESC;")
+
+# Convert to dataframe
+opensourcer_df <- as.data.frame(opensourcer)
+
+# Plot data
+ggplot(opensourcer_df, aes(x = reorder(OpenSourcer, Amount), y = Amount)) +
+  geom_bar(stat = "identity", color = "#5AA2F9", fill = "#8BBEFB", width = 0.8) + 
+  coord_flip() + 
+  theme_minimal() +
+  labs(title = "Contributing to Open Source",
+       subtitle = sum(opensourcer_df$Amount) %>%
+         format(big.mark = ".", decimal.mark = ",") %>%
+         paste("All Respondents (", ., ")", sep = "")) +
+  theme(plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5),
+        axis.title.y = element_blank()) +
+  scale_y_continuous(limits = c(0, 35000), breaks = seq(0, 35000, 10000), expand = c(0, 0), labels = scales::number_format(big.mark = ".", decimal.mark = ",")) +
+  geom_text(aes(label = (Amount/sum(Amount)) %>% scales::percent(decimal.mark = ",")), hjust = -0.1)
+
+# Disconnect database
+dbDisconnect(con)
